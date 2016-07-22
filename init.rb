60eb4fd4d10d150bcc77f0527bc4fb447aa7d266
ocaisa <a.ocais@fz-juelich.de>
2016-07-22 17:43:58 +0200

    Update init.rb

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require_dependency 'redmine_omniauth_gitlab/hooks'
 
 Redmine::Plugin.register :redmine_omniauth_gitlab do
   name 'Redmine Omniauth GitLab plugin'
-  author 'Alan OCais (Original author Dmitry Kovalenok, his URL below)'
+  author 'Alan OCais (Original author Dmitry Kovalenok, his URL linked)'
   description 'This is a plugin for Redmine registration through GitLab'
   version '0.0.1'
   url 'https://github.com/ocaisa/redmine_omniauth_gitlab'
