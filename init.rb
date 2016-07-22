80c6ee9f1fdc8e04283824baf99ea6844fd8c67c
ocaisa <a.ocais@fz-juelich.de>
2016-07-22 17:39:34 +0200

    Update init.rb
    Change author but make sure Dmitry gets the credit

--- init.rb
+++ init.rb
@@ -3,10 +3,10 @@ require_dependency 'redmine_omniauth_gitlab/hooks'
 
 Redmine::Plugin.register :redmine_omniauth_gitlab do
   name 'Redmine Omniauth GitLab plugin'
-  author 'Dmitry Kovalenok'
+  author 'Alan OCais (Original author Dmitry Kovalenok, his URL below)'
   description 'This is a plugin for Redmine registration through GitLab'
   version '0.0.1'
-  url 'https://github.com/twinslash/redmine_omniauth_google'
+  url 'https://github.com/ocaisa/redmine_omniauth_gitlab'
   author_url 'http://twinslash.com'
 
   settings :default => {
